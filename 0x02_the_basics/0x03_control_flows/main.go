82fdf4892c8736e50c694b235b0e5373fd54ba7a
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2025-09-29 00:55:38 +0200

    0x03 - Added if-else, switches, loops & defer

---
+++ 0x02_the_basics/0x03_control_flows/main.go
@@ -0,0 +1,119 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	isTrue := true
+	isFalse := false
+
+	// Defer to block end
+	defer fmt.Println("Run at the end!")
+
+	// True even if the first case matches
+	if isTrue || isFalse == true {
+		fmt.Println("I am true, even if the first statement matches")
+	}
+
+	// If statements are simple
+	if isTrue && isFalse {
+		fmt.Println("Both true")
+	} else if isTrue && !isFalse {
+		fmt.Println("First true, second false!")
+	} else {
+		fmt.Println("Other")
+	}
+
+	// Swicthes execute in order and are not limited to integers
+	// https://github.com/mitchelkoster/vacay-gaurdian/blob/1.0.0/utils/storage.go
+	fmt.Print("The system is running....")
+	switch os := runtime.GOOS; os {
+	case "windows":
+		fmt.Println("macOS")
+	case "linux":
+		fmt.Println("Linux")
+	case "darwin":
+		fmt.Println("Mac")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+
+	// You can do calculations in cases
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+	// Switches don't need conditions and can act
+	// like long if-else chains
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	// Switches can have multiple conditions
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// WHILE
+	i := 0
+	for i < 5 {
+		fmt.Print(i)
+		i++
+	}
+	fmt.Println()
+
+	// ENDLESS
+	// for {
+	// 	fmt.Println("Hello world!")
+	// }
+
+	// FOR loop
+	for i := 5; i < 10; i++ {
+		fmt.Print(i)
+	}
+	fmt.Println()
+
+	// Ranges (basic)
+	for i := range 5 {
+		fmt.Print(i)
+	}
+	fmt.Println()
+
+	// Ranges apply to built-in types (foreach)
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	// Defers are stacked function calls (event loop-like)
+	// It's essentially a "finally" exception block
+	// Can help prevent deadlocks
+	fmt.Println("counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+}
