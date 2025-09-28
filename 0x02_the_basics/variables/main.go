46e6a5685de99e5537868ac0149151647624d99b
Mitchel Koster <2762686+mitchelkoster@users.noreply.github.com>
2025-09-28 23:19:55 +0200

    Added some more notes to variables

--- 0x02_the_basics/variables/main.go
+++ 0x02_the_basics/variables/main.go
@@ -5,6 +5,8 @@ package main
 
 import "fmt"
 
+var globalMessage string = "Global message: initial"
+
 func main() {
 	var name string  // Declared, not initalized
 	name = "Mitchel" // Initialized
@@ -18,13 +20,32 @@ func main() {
 	fmt.Println("Hello,", name, age)
 
 	// Formatted prints look good!
-	fmt.Printf("Hello, %s (age %d)!\n", name, age)
+	fmt.Printf("Hello, %s (%T) %d (%T)\n", name, name, age, age)
 
 	var a, b, c int = 1, 2, 3
 	fmt.Println(a, b, c)
 
 	// The := syntax is shorthand for declaring and
-	// initializing a
+	// initializing a variable
 	message := "Golang is kinda cool!"
 	fmt.Println(message)
+
+	// Const, cannot be  overwritten (high precision calculation)
+	const VERSION = "v1.0.0"
+	// VERSION = "v1.0.0" // Errors
+	fmt.Println("Program version: ", VERSION)
+
+	// Overwrites globalmessage
+	// globalMessage = "Gobal message: updated"
+
+	// Creates a new global variable, but only in the
+	// scope of this function
+	var globalMessage string = "Global mesage: scope"
+	fmt.Println(globalMessage)
+
+	test()
+}
+
+func test() {
+	fmt.Println(globalMessage)
 }
